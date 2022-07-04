0ce2dd62b8af2913ab0aed54d1e62061935dcf97
aparolar <aparolar@student.42madrid.com>
2022-07-04 21:21:55 +0200

    empezando ex01

---
+++ ex01/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aparolar <aparolar@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/02 17:34:20 by aparolar          #+#    #+#             */
+/*   Updated: 2022/07/04 20:27:07 by aparolar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <ctype.h>
+
+int	main(int argc, char **argv)
+{
+	std::string	str;
+
+	while (42)
+	{
+		//std::getline(std::cin, str);
+		std::cin >> str;
+		switch (std::string(str))
+		{
+			case 
+		}
+	}
+	return (0);
+}
