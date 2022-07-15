6f8b533f9d9a4f1dae5208b90b95b816a494a92c
aparolar <aparolar@student.42madrid.com>
2022-07-15 17:08:43 +0200

    cpp00/ex02 finished

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,69 +6,185 @@
 /*   By: aparolar <aparolar@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 16:46:30 by aparolar          #+#    #+#             */
-/*   Updated: 2022/07/13 17:54:43 by aparolar         ###   ########.fr       */
+/*   Updated: 2022/07/15 17:07:28 by aparolar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 
+int	Account::nbAccounts = 0;
+int	Account::totalAmount = 0;
+int	Account::totalNbDeposits = 0;
+int	Account::totalNbWhitdrawals = 0;
+
 Account::Account(int initialDeposit)
 {
-	nbAccounts = 0;
-	totalAmount = 0;
-	totalNbDeposits = 0;
-	totalNbWhitdrawals = 0;
+	this->p_amount = 0;
+	this->nbDeposits = 0;
+	this->nbWithdrawals = 0;
+	this->index = this->nbAccounts++;
+	this->amount = initialDeposit;
+	this->totalAmount += initialDeposit;
+	std::cout << getTimestamp();
+	std::cout << displayIndex() << ";";
+	std::cout << displayAmount() << ";";
+	std::cout << TERM_GREEN << "created" << TERM_RST << std::endl;
 }
 
 Account::~Account()
 {
+	std::cout << getTimestamp();
+	std::cout << displayIndex() << ";";
+	std::cout << displayAmount() << ";";
+	std::cout << TERM_RED << "closed" << TERM_RST << std::endl;
+}
+
+std::string	Account::getTimestamp( void )
+{
+	return ("[19920104_091532] ");
 }
 
 void	Account::displayAccountsInfos( void )
 {
-	
+	std::cout << Account::getTimestamp();
+	std::cout << Account::displayTotalAccounts() << ";";
+	std::cout << Account::displayTotalAmount() << ";";
+	std::cout << Account::displayTotalDeposits() << ";";
+	std::cout << Account::displayTotalWithdrawals() << std::endl;
 }
 
 void	Account::displayStatus( void ) const
 {
-	//Account::_displayTimestamp();
-	std::cout << "index:" << this->accountIndex << ";";
-	std::cout << "amount:" << Account::checkAmount() << ";";
-	std::cout << "deposits:" << this->nbDeposits << ";";
-	std::cout << "withdrawals:" << this->nbWithdrawals << std::endl;
+	std::cout << Account::getTimestamp();
+	std::cout << displayIndex() << ";";
+	std::cout << displayAmount() << ";";
+	std::cout << displayDeposits() << ";";
+	std::cout << displayWihdrawals() << std::endl;
+}
+
+std::string	Account::getColumnAndValue(std::string column, int value)
+{
+	std::string			ret;
+	std::stringstream	ss;
+
+	ss << TERM_ORANGE << column << ":" << TERM_BLUE << value << TERM_RST;
+	ss >> ret;
+	return (ret);
+}
+
+std::string	Account::getColumnAndValue(std::string column, std::string value)
+{
+	std::string			ret;
+	std::stringstream	ss;
+
+	ss << TERM_ORANGE << column << ":" << TERM_BLUE << value << TERM_RST;
+	ss >> ret;
+	return (ret);
+}
+
+std::string	Account::displayIndex( void ) const
+{
+	return (getColumnAndValue("index", this->index));
+}
+
+std::string	Account::displayAmount( void ) const
+{
+	return (getColumnAndValue("amount", this->amount));
+}
+
+std::string	Account::displayDeposits( void ) const
+{
+	return (getColumnAndValue("deposits", this->nbDeposits));
+}
+
+std::string Account::displayWihdrawals( void ) const
+{
+	return (getColumnAndValue("withdrawals", this->nbWithdrawals));
+}
+
+std::string	Account::displayTotalAmount( void )
+{
+	return (getColumnAndValue("total", Account::getTotalAmount()));
+}
+
+std::string	Account::displayTotalDeposits( void )
+{
+	return (getColumnAndValue("deposits", Account::getTotalNbDeposits()));
+}
+
+std::string Account::displayTotalWithdrawals( void )
+{
+	return (getColumnAndValue("withdrawals", Account::getTotalNbWhitdrawals()));
+}
+
+std::string Account::displayTotalAccounts( void )
+{
+	return (getColumnAndValue("accounts", Account::getNbAccounts()));
 }
 
 int		Account::checkAmount( void ) const
 {
-	
+	return (this->amount);	
 }
 
 void	Account::makeDeposit( int deposit )
 {
-	
+	this->p_amount = this->amount;
+	std::cout << Account::getTimestamp();
+	std::cout << displayIndex() << ";";
+	std::cout << getColumnAndValue("p_amount", this->p_amount) << ";";
+	if (deposit > 0)
+	{
+		this->amount += deposit;
+		this->totalAmount += deposit;
+		this->nbDeposits++;
+		this->totalNbDeposits++;
+		std::cout << getColumnAndValue("deposit", deposit) << ";";
+		std::cout << displayAmount() << ";";
+		std::cout << getColumnAndValue("nb_deposits", nbDeposits) << ";";
+	}
+	else
+		std::cout << getColumnAndValue("deposit", "refused");
+	std::cout << std::endl;
 }
 
 bool	Account::makeWithdrawal( int withdrawal )
 {
+
+	std::cout << Account::getTimestamp();
+	std::cout << displayIndex() << ";";
+	std::cout << getColumnAndValue("p_amount", amount) << ";";
+	if (amount >= withdrawal)
+	{
+		amount -= withdrawal;
+		totalAmount -= withdrawal;
+		nbWithdrawals++;
+		std::cout << getColumnAndValue("withdrawal", withdrawal) << ";";
+		std::cout << displayAmount() << ";";
+		std::cout << getColumnAndValue("nb_withdrawals", nbWithdrawals) << ";";
+	}
+	else
+		std::cout << getColumnAndValue("withdrawal", "refused");
+	std::cout << std::endl;
 	return (true);
 }
 
 int		Account::getNbAccounts()
 {
-	return (this->nbAccounts);
+	return (Account::nbAccounts);
 }
 
 int		Account::getTotalAmount()
 {
-	return (this->totalAmount);
+	return (Account::totalAmount);
 }
 
 int		Account::getTotalNbDeposits()
 {
-	return (this->totalNbDeposits);
+	return (Account::totalNbDeposits);
 }
 
 int		Account::getTotalNbWhitdrawals()
 {
-	return (this->totalNbWhitdrawals);
+	return (Account::totalNbWhitdrawals);
 }
--- ex02/Account.hpp
+++ ex02/Account.hpp
@@ -6,7 +6,7 @@
 /*   By: aparolar <aparolar@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 16:46:49 by aparolar          #+#    #+#             */
-/*   Updated: 2022/07/13 17:53:10 by aparolar         ###   ########.fr       */
+/*   Updated: 2022/07/15 16:10:46 by aparolar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,28 @@
 
 # include <iostream>
 # include <streambuf>
+# include <sstream>
+# include <string>
+
+# define TERM_RED     "\033[31m"
+# define TERM_GREEN   "\033[32m"
+# define TERM_ORANGE  "\033[33m"
+# define TERM_BLUE    "\033[34m"
+# define TERM_RST     "\033[0m"
 
 class Account
 {
 	private:
-		int	nbAccounts;
-		int	totalAmount;
-		int	totalNbDeposits;
-		int	totalNbWhitdrawals;
+		static int	nbAccounts;
+		static int	totalAmount;
+		static int	totalNbDeposits;
+		static int	totalNbWhitdrawals;
 
+		int p_amount;
+		int	amount;
 		int	nbDeposits;
 		int	nbWithdrawals;
-		int	accountIndex;
+		int	index;
 
 	public:
 		typedef Account	t;
@@ -38,13 +48,25 @@ class Account
 		bool	makeWithdrawal( int withdrawal );
 		int		checkAmount( void ) const;
 		void	displayStatus( void ) const;
+		static std::string	getColumnAndValue(std::string column, int value);
+		static std::string	getColumnAndValue(std::string column, std::string value);
+		std::string	displayIndex( void ) const;
+		std::string	displayAmount( void ) const;
+		std::string	displayDeposits( void ) const;
+		std::string	displayWihdrawals( void ) const;
 
-		int		getNbAccounts( void );
-		int		getTotalAmount( void );
-		int		getTotalNbDeposits( void );
-		int		getTotalNbWhitdrawals( void );
+		static std::string	displayTotalAmount( void );
+		static std::string	displayTotalDeposits( void );
+		static std::string displayTotalWithdrawals( void );
+		static std::string	displayTotalAccounts( void );
+
+		static int		getNbAccounts( void );
+		static int		getTotalAmount( void );
+		static int		getTotalNbDeposits( void );
+		static int		getTotalNbWhitdrawals( void );
 		
-		static	void 	displayAccountsInfos( void );
+		static	void 		displayAccountsInfos( void );
+		static	std::string	getTimestamp( void );
 };
 
 #endif
